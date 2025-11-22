d77b624b16f5f31f21d8034a3942e61770f879c0
rootmium <rootmium@gmail.com>
2025-11-23 00:27:17 +0600

    create a basic express server
    subtext

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-console.log("server file is great!")
+console.log("server file is great!");
+import express from "express";
+const app = express();
+
+// Start listening
+app.listen(3000, () => {
+  console.log("Server is running at http://localhost:3000");
+});
