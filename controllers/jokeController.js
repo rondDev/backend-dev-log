c9b49a5fb6eb72bbec2616dc97813f8f4880583e
rootmium <rootmium@gmail.com>
2025-11-26 02:03:22 +0600

    adding try catch blocks

--- controllers/jokeController.js
+++ controllers/jokeController.js
@@ -1,35 +1,43 @@
 import { prisma } from "../lib/prisma.js";
 
 export const getAllJokes = async (_req, res) => {
-  const jokes = await prisma.joke.findMany();
-  res.json(jokes);
+    try {
+        const jokes = await prisma.joke.findMany();
+        res.status(200).json(jokes);
+    } catch (err) {
+        res.status(500).json({ error: err });
+    }
 };
 
 const selectJokeById = async (id) => {
-  const joke = await prisma.joke.findUnique({
-    where: { id: Number.parseInt(id) },
-  });
-  return joke;
+    const joke = await prisma.joke.findUnique({
+        where: { id: Number.parseInt(id) },
+    });
+    return joke;
 };
 
 export const getJokeById = async (req, res) => {
-  const requestId = req.params.id;
-  const joke = await selectJokeById(requestId);
-  res.json(joke);
+    try {
+        const requestId = req.params.id;
+        console.log(Number.isInteger(Number.parseInt(requestId)));
+        // const joke = await selectJokeById(requestId);
+        // res.json(joke);
+        res.send("Done");
+    } catch (err) {}
 };
 
 export const getRandomJoke = async (_req, res) => {
-  const totalJokes = await prisma.joke.count();
-  const randomId = Math.floor(Math.random() * totalJokes);
-  const joke = await selectJokeById(randomId);
-  res.status(200).json(joke);
+    const totalJokes = await prisma.joke.count();
+    const randomId = Math.floor(Math.random() * totalJokes);
+    const joke = await selectJokeById(randomId);
+    res.status(200).json(joke);
 };
 
 export const createJoke = async (req, res) => {
-  const text = req.body.text;
+    const text = req.body.text;
 
-  const newJoke = await prisma.joke.create({
-    data: { text },
-  });
-  res.status(201).json(newJoke);
+    const newJoke = await prisma.joke.create({
+        data: { text },
+    });
+    res.status(201).json(newJoke);
 };
