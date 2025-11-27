7bac3ef4b0a474aa36a23d4cd2ac6bad5d5dd1b5
rootmium <rootmium@gmail.com>
2025-11-28 00:16:15 +0600

    feat: sql queries for better databse integration

--- controllers/jokeController.js
+++ controllers/jokeController.js
@@ -1,33 +1,55 @@
+import { DatabaseSync } from "node:sqlite";
+const db = new DatabaseSync("./dev.db");
+const sql = db.createTagStore();
 
+export const getAllJokes = (_req, res) => {
+  try {
+    const jokes = sql.all`SELECT * FROM Joke`
+    res.status(200).json(jokes);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
 };
 
+const selectJokeById = (id) => {
+  const joke = sql.get`SELECT id, text FROM Joke WHERE id = ${id}`
+  return joke;
 };
 
+export const getJokeById = (req, res) => {
     try {
         const requestId = req.params.id;
         if (!Number.isInteger(Number.parseInt(requestId))) {
             return res.status(400).json({ error: "Bad request" });
         }
 
+        const joke = selectJokeById(requestId);
         res.status(200).json(joke);
     } catch (err) {
         res.status(500).json({ error: err });
     }
 };
 
+export const getRandomJoke = (_req, res) => {
     try {
+      const joke = sql.all`SELECT * FROM Joke ORDER BY random() LIMIT 1`
         res.status(200).json(joke);
     } catch (err) {
         res.status(500).json({ error: err });
     }
 };
 
+export const createJoke = (req, res) => {
     try {
         const text = req.body.text;
         if (!text) {
             return res.status(400).json({ error: "text doesn't exist" });
         }
 
+      const insertJoke = db.prepare("INSERT INTO Joke (text) VALUES (?)")
+      insertJoke.run(text)
+
+      res.status(201).json({message: "Added"});
     } catch (err) {
         return res.status(500).json({ error: err });
     }
--- seed.ts
+++ seed.ts
@@ -2,13 +2,13 @@ import { DatabaseSync } from "node:sqlite";
 import { existsSync, rmSync } from "node:fs";
 import file from "./data/jokes.json" with { type: "json" };
 
-if (existsSync("./test.db")) {
-  rmSync("./test.db");
+if (existsSync("./dev.db")) {
+  rmSync("./dev.db");
 }
-const db = new DatabaseSync("./test.db");
-db.exec(`CREATE TABLE Joke(id INTEGER PRIMARY KEY, joke TEXT)`);
+const db = new DatabaseSync("./dev.db");
+db.exec(`CREATE TABLE Joke(id INTEGER PRIMARY KEY, text TEXT)`);
 
-const ins = db.prepare("INSERT INTO Joke (id, joke) VALUES (?, ?)");
+const ins = db.prepare("INSERT INTO Joke (id, text) VALUES (?, ?)");
 file.forEach((s) => {
   ins.run(s.id, s.joke);
 });
